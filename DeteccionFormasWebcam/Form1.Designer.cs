7559365d6fccad2eaee3d60d3413f00951f8c4aa
David Chavez <david_12-95@hotmail.com>
2018-11-19 21:44:02 -0700

    Add project files.

---
+++ DeteccionFormasWebcam/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace DeteccionFormasWebcam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.webcamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detenerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detectarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.webcamToolStripMenuItem,
+            this.detectarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // webcamToolStripMenuItem
+            // 
+            this.webcamToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarToolStripMenuItem,
+            this.detenerToolStripMenuItem});
+            this.webcamToolStripMenuItem.Name = "webcamToolStripMenuItem";
+            this.webcamToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.webcamToolStripMenuItem.Text = "Webcam";
+            // 
+            // iniciarToolStripMenuItem
+            // 
+            this.iniciarToolStripMenuItem.Name = "iniciarToolStripMenuItem";
+            this.iniciarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.iniciarToolStripMenuItem.Text = "Iniciar";
+            this.iniciarToolStripMenuItem.Click += new System.EventHandler(this.iniciarToolStripMenuItem_Click);
+            // 
+            // detenerToolStripMenuItem
+            // 
+            this.detenerToolStripMenuItem.Name = "detenerToolStripMenuItem";
+            this.detenerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.detenerToolStripMenuItem.Text = "Detener";
+            this.detenerToolStripMenuItem.Click += new System.EventHandler(this.detenerToolStripMenuItem_Click);
+            // 
+            // detectarToolStripMenuItem
+            // 
+            this.detectarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarToolStripMenuItem1});
+            this.detectarToolStripMenuItem.Name = "detectarToolStripMenuItem";
+            this.detectarToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.detectarToolStripMenuItem.Text = "Detectar";
+            // 
+            // iniciarToolStripMenuItem1
+            // 
+            this.iniciarToolStripMenuItem1.Name = "iniciarToolStripMenuItem1";
+            this.iniciarToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.iniciarToolStripMenuItem1.Text = "Iniciar";
+            this.iniciarToolStripMenuItem1.Click += new System.EventHandler(this.iniciarToolStripMenuItem1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(380, 410);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(399, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(389, 410);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem webcamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iniciarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detenerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem detectarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iniciarToolStripMenuItem1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ DeteccionFormasWebcam/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace DeteccionFormasWebcam
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+        Image<Bgr, byte> imgInput;
+
+        public Form1()
+        {
+            InitializeComponent();
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 1000;
+            aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            if (capture == null)
+            {
+                return;
+            }
+            DetectarForma();
+        }
+
+        private void iniciarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                capture = new Emgu.CV.VideoCapture(0);
+            }
+            capture.ImageGrabbed += CaptureImageGrabbed;
+            capture.Start();
+        }
+
+        private void CaptureImageGrabbed(object sender, EventArgs e)
+        {
+            try
+            {
+                Mat m = new Mat();
+                capture.Retrieve(m);
+                pictureBox1.Image = m.ToImage<Bgr, byte>().Bitmap;
+                imgInput = new Image<Bgr, byte>(m.ToImage<Bgr, byte>().Bitmap);
+
+                if (capture == null)
+                {
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void detenerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (capture != null)
+            {
+                capture.Stop();
+            }
+        }
+
+        private void iniciarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                return;
+            }
+
+            try
+            {
+                var temp = imgInput.SmoothGaussian(1).Convert<Gray, byte>().ThresholdBinaryInv(new Gray(230), new Gray(255));
+
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                Mat m = new Mat();
+
+                CvInvoke.FindContours(temp, contours, m, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    double perimeter = CvInvoke.ArcLength(contours[i], true);
+                    VectorOfPoint approx = new VectorOfPoint();
+                    CvInvoke.ApproxPolyDP(contours[i], approx, 0.04 * perimeter, true);
+
+                    CvInvoke.DrawContours(imgInput, contours, i, new MCvScalar(0, 0, 255), 2);
+
+                    var moments = CvInvoke.Moments(contours[i]);
+                    int x = (int)(moments.M10 / moments.M00);
+                    int y = (int)(moments.M01 / moments.M00);
+
+                    if (approx.Size == 3)
+                    {
+                        CvInvoke.PutText(imgInput, "Triangulo", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                    if (approx.Size == 4)
+                    {
+                        Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+
+                        double ar = (double)rect.Width / rect.Height;
+
+                        if (ar >= 0.95 && ar <= 1.05)
+                        {
+                            CvInvoke.PutText(imgInput, "Cuadrado", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                        }
+                        else
+                        {
+                            CvInvoke.PutText(imgInput, "Rectangulo", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                        }
+                    }
+
+                    if (approx.Size == 5)
+                    {
+                        CvInvoke.PutText(imgInput, "Pentagono", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                    if (approx.Size == 6)
+                    {
+                        CvInvoke.PutText(imgInput, "Hexagono", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                    if (approx.Size == 7)
+                    {
+                        CvInvoke.PutText(imgInput, "Heptagono", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                    if (approx.Size > 7)
+                    {
+                        CvInvoke.PutText(imgInput, "Circulo", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                    pictureBox2.Image = imgInput.Bitmap;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void DetectarForma()
+        {
+            if (capture == null)
+            {
+                return;
+            }
+
+            try
+            {
+                var temp = imgInput.SmoothGaussian(3).Convert<Gray, byte>().ThresholdBinaryInv(new Gray(230), new Gray(255));
+
+                VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+                Mat m = new Mat();
+
+                CvInvoke.FindContours(temp, contours, m, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    double perimeter = CvInvoke.ArcLength(contours[i], true);
+                    VectorOfPoint approx = new VectorOfPoint();
+                    CvInvoke.ApproxPolyDP(contours[i], approx, 0.04 * perimeter, true);
+
+                    CvInvoke.DrawContours(imgInput, contours, i, new MCvScalar(0, 0, 255), 2);
+
+                    var moments = CvInvoke.Moments(contours[i]);
+                    int x = (int)(moments.M10 / moments.M00);
+                    int y = (int)(moments.M01 / moments.M00);
+
+                    if (approx.Size == 3)
+                    {
+                        CvInvoke.PutText(imgInput, "Triangulo", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                    if (approx.Size == 4)
+                    {
+                        Rectangle rect = CvInvoke.BoundingRectangle(contours[i]);
+
+                        double ar = (double)rect.Width / rect.Height;
+
+                        if (ar >= 0.95 && ar <= 1.05)
+                        {
+                            CvInvoke.PutText(imgInput, "Cuadrado", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                        }
+                        else
+                        {
+                            CvInvoke.PutText(imgInput, "Rectangulo", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                        }
+                    }
+
+                    if (approx.Size == 5)
+                    {
+                        CvInvoke.PutText(imgInput, "Pentagono", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                    if (approx.Size == 6)
+                    {
+                        CvInvoke.PutText(imgInput, "Hexagono", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                    if (approx.Size == 7)
+                    {
+                        CvInvoke.PutText(imgInput, "Heptagono", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                    if (approx.Size > 7)
+                    {
+                        CvInvoke.PutText(imgInput, "Circulo", new Point(x, y), FontFace.HersheySimplex, 0.5, new MCvScalar(0, 0, 255), 2);
+                    }
+
+                    pictureBox2.Image = imgInput.Bitmap;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
